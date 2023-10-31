e4bc599acbfb4ed010c5e3844693e8569e3bed87
murilonicemento <murilo_nascimento22@outlook.com>
2023-10-31 20:30:12 -0300

    feat: inclusão componente form na page register

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,9 +1,11 @@
+import { Form } from "../components/Form.tsx";
 import { Header } from "../components/Header.tsx";
 
 export function Register() {
   return (
-    <div>
+    <>
       <Header />
-    </div>
+      <Form />
+    </>
   );
 }
