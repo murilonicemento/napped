0b4be9ee5a84ea396c22430b8bd9bab12c4d5fd5
murilonicemento <murilo_nascimento22@outlook.com>
2023-11-02 17:04:01 -0300

    feat: inclusão de funcionalidade que ao clicar na logo ser redirecionado para a página principal

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,16 @@
+import { Link } from "react-router-dom";
 import logo from "../assets/logo.svg";
 import { PopAnchor } from "./Popover.tsx";
 
 export function Header() {
   return (
     <header className="w-full border border-solid border-b-dark-30 flex justify-between p-4">
-      <div className="flex gap-4 items-center justify-center">
+      <Link to="/" className="flex gap-4 items-center justify-center">
         <img src={logo} alt="Logo" className="w-10" />
         <span className="text-transparent bg-clip-text bg-gradient-to-b from-brand to-dark-blue text-dark-blue font-medium text-2xl">
           Napped
         </span>
-      </div>
+      </Link>
       <PopAnchor />
     </header>
   );
