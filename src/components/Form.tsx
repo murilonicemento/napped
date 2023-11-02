2ae7fb2859b69d47f570249836334718d275fa21
murilonicemento <murilo_nascimento22@outlook.com>
2023-11-02 17:03:15 -0300

    feat: finalização da validação do formulário

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,59 +1,94 @@
-import axios from "axios";
+import axios, { AxiosError } from "axios";
 import { useState } from "react";
 import toast from "react-hot-toast";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { DataAPI, ErrorAPI } from "../interfaces/types";
 
 export function Form() {
   const [name, setName] = useState<string>("");
   const [email, setEmail] = useState<string>("");
   const [password, setPassword] = useState<string>("");
   const [passwordRepeat, setPasswordRepeat] = useState<string>("");
+  const navigate = useNavigate();
 
-  async function handleSubmit(event: { preventDefault: () => void }) {
-    event?.preventDefault();
+  function handlePaste(event: { preventDefault: () => void }) {
+    event.preventDefault();
+    toast.error(
+      "Por favor, digite sua senha novamente. A ação de colar está bloqueada."
+    );
+  }
+
+  function validateForm() {
+    let isValid = true;
+    const passwordRegex =
+      /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[$*&@#])[0-9a-zA-Z$*&@#]{8,}$/;
 
     if (
-      name === "" ||
-      email === "" ||
-      password === "" ||
-      passwordRepeat === ""
+      !name.trim().length ||
+      !email.trim().length ||
+      !password.trim().length ||
+      !passwordRepeat.trim().length
     ) {
-      return toast.error("Por favor, preencha todos os campos.", {
-        duration: 3000,
-        position: "top-center",
-        style: {
-          backgroundColor: "#151B26",
-          color: "#FEFBFB",
-        },
-      });
+      toast.error("Por favor, preencha todos os campos.");
+      isValid = false;
+      return isValid;
+    }
+
+    if (!passwordRegex.test(password)) {
+      toast.error(
+        "Senha deve conter 8 caracteres, letras maiúsculas e minúsculas e um carácter especial.",
+        { duration: 5000 }
+      );
+      isValid = false;
+      return isValid;
     }
 
     if (password !== passwordRepeat) {
-      return toast.error("Senhas não coincidem.", {
-        duration: 3000,
-        position: "top-center",
-        style: {
-          backgroundColor: "#151B26",
-          color: "#FEFBFB",
-        },
-      });
+      toast.error("Senhas não coincidem.");
+      isValid = false;
+      return isValid;
     }
+  }
 
-    const data = await axios.post(
-      "http://localhost:8080/api/register",
-      {
-        name,
-        email,
-        password,
-      },
-      {
-        headers: {
-          "Content-Type": "multipart/form-data",
+  async function handleSubmit(event: { preventDefault: () => void }) {
+    event.preventDefault();
+
+    if (!validateForm()) return;
+
+    try {
+      const toastId = toast.loading("Criando usuário...");
+      const { data } = await axios.post<DataAPI>(
+        "http://localhost:8080/api/register",
+        {
+          name,
+          email,
+          password,
         },
-      }
-    );
+        {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+        }
+      );
 
-    console.log(data);
+      setTimeout(() => {
+        toast.success(data.message, {
+          id: toastId,
+          position: "top-center",
+          style: {
+            backgroundColor: "#151B26",
+            color: "#FEFBFB",
+          },
+        });
+      }, 1300);
+
+      return setTimeout(() => navigate("/"), 2200);
+    } catch (error) {
+      const data = (error as AxiosError<ErrorAPI>).response?.data;
+      const message = data?.error.message;
+
+      return toast.error(`${message}`);
+    }
   }
 
   return (
@@ -74,7 +109,7 @@ export function Form() {
           placeholder="Digite seu nome"
           value={name}
           onChange={(event) => setName(event.target.value)}
-          className="w-full bg-user bg-left bg-no-repeat bg-dark-30 pl-10 pt-2 pb-2 border border-solid rounded-md border-dark-30 text-white placeholder:text-white outline-none"
+          className="w-full bg-[url('./src/assets/user.svg')] bg-left bg-no-repeat bg-dark-30 pl-10 pt-2 pb-2 border border-solid rounded-md border-dark-30 text-white placeholder:text-white"
         />
       </label>
       <label htmlFor="email" className="w-full">
@@ -85,7 +120,7 @@ export function Form() {
           placeholder="Digite seu e-mail"
           value={email}
           onChange={(event) => setEmail(event.target.value)}
-          className="w-full bg-at-sign bg-left bg-no-repeat bg-dark-30 pl-10 pt-2 pb-2 border rounded-md border-dark-30 text-white placeholder:text-white outline-none"
+          className="w-full bg-[url('./src/assets/at-sign.svg')] bg-left bg-no-repeat bg-dark-30 pl-10 pt-2 pb-2 border rounded-md border-dark-30 text-white placeholder:text-white"
         />
       </label>
       <label htmlFor="password" className="w-full">
@@ -96,7 +131,7 @@ export function Form() {
           placeholder="Digite uma senha"
           value={password}
           onChange={(event) => setPassword(event.target.value)}
-          className="w-full bg-lock bg-left bg-no-repeat bg-dark-30 pl-10 pt-2 pb-2 border rounded-md border-dark-30 text-white placeholder:text-white outline-none"
+          className="w-full bg-[url('./src/assets/lock.svg')] bg-left bg-no-repeat bg-dark-30 pl-10 pt-2 pb-2 border rounded-md border-dark-30 text-white placeholder:text-white"
         />
       </label>
       <label htmlFor="passwordRepeat" className="w-full">
@@ -107,7 +142,8 @@ export function Form() {
           placeholder="Digite novamente a senha"
           value={passwordRepeat}
           onChange={(event) => setPasswordRepeat(event.target.value)}
-          className="w-full bg-lock bg-left bg-no-repeat bg-dark-30 pl-10 pt-2 pb-2 border rounded-md border-dark-30 text-white placeholder:text-white outline-none"
+          onPaste={handlePaste}
+          className="w-full bg-[url('./src/assets/lock.svg')] bg-left bg-no-repeat bg-dark-30 pl-10 pt-2 pb-2 border rounded-md border-dark-30 text-white placeholder:text-white"
         />
       </label>
 
