fe71f2966a54593855f84e877e79d8155799fcea
murilonicemento <murilo_nascimento22@outlook.com>
2023-11-02 17:02:25 -0300

    feat: criação de interfaces para os dados que vem da api ao realizar o registro de usuário

--- src/interfaces/types.ts
+++ src/interfaces/types.ts
@@ -4,3 +4,20 @@ export interface FormProps {
   password: string;
   passwordRepeat: string;
 }
+
+export interface DataAPI {
+  user: {
+    id: number;
+    name: string;
+    email: string;
+  };
+  message: string;
+  statusCode: number;
+}
+
+export interface ErrorAPI {
+  error: {
+    message: string;
+  };
+  statusCode: number;
+}
