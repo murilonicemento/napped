f3000a3c4b1ba82a8e63c403e6ecb440875a6d05
murilonicemento <murilo_nascimento22@outlook.com>
2023-11-03 19:01:37 -0300

    feat: inclusão de novos textos e reestruturação dos elementos

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -6,24 +6,28 @@ export function NotFound() {
   return (
     <>
       <Header />
-      <main className="w-10/12 m-auto mt-20 text-white">
+      <main className="w-10/12 m-auto mt-16 text-white">
+        <section className="grid place-items-center mb-8">
+          <img src={notFound} alt="404 Not Found Image" />
+          <p className="mt-10">Ups! Você chegou no espaço...</p>
+          <p>volte para o mundo nerd!</p>
+        </section>
+
         <section className="flex flex-col justify-center mb-6">
-          <h1 className="text-2xl text-center mb-6">
+          <h1 className="text-3xl text-center mb-6">
             Tenho más notícias para você!
           </h1>
           <p className="text-justify mb-6">
             A página que você está procurando pode ter sido removida ou está
             temporariamente indisponível.
           </p>
-          <img src={notFound} alt="404 Not Found Image" />
           <Link
             to="/"
-            className="bg-gradient-to-b from-brand to-dark-blue w-full flex justify-center border rounded border-none p-2 mt-12"
+            className="bg-gradient-to-b from-brand to-dark-blue w-full flex justify-center border rounded border-none p-2"
           >
             Voltar para o início
           </Link>
         </section>
-        <section></section>
       </main>
     </>
   );
