1910d0b41827343b50f7e3145c91b4a977012988
murilonicemento <murilo_nascimento22@outlook.com>
2023-11-03 18:51:13 -0300

    feat(routes): inclusão de rotas de not found e página inicial

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import { Toaster } from "react-hot-toast";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Home } from "./pages/Home.tsx";
+import { NotFound } from "./pages/NotFound.jsx";
 import { Register } from "./pages/Register.tsx";
 
 export function App() {
@@ -7,7 +9,9 @@ export function App() {
     <>
       <BrowserRouter>
         <Routes>
+          <Route path="/" element={<Home />} />
           <Route path="/register" element={<Register />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
       <Toaster
