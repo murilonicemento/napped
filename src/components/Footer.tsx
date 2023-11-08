4f6a0a14b88c204096d97d89e0e8ca2c944904b9
murilonicemento <murilo_nascimento22@outlook.com>
2023-11-08 18:48:34 -0300

    feat: criação componente footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,13 @@
+import brandLogo from "../assets/brand-logo.svg";
+
+export function Footer() {
+  return (
+    <footer className="w-full p-4 mt-4 bg-footer text-dark-40 flex flex-col items-center justify-center gap-1">
+      <p>Copyright © 2023. Todos os direitos reservados.</p>
+      <div className="flex items-center justify-center gap-2">
+        <p>Powered by</p>
+        <img src={brandLogo} alt="Brand Logo" />
+      </div>
+    </footer>
+  );
+}
