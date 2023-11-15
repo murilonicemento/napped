fd991f87a2bf39e24628d9e1d73243de7863ac6c
murilonicemento <murilo_nascimento22@outlook.com>
2023-11-15 11:34:27 -0300

    feat: inclusão docontexto global e do auth provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,76 @@
 import { Toaster } from "react-hot-toast";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { Home } from "./pages/Home.tsx";
+import { AuthProvider } from "./context/AuthProvider.tsx";
+import { RequireAuth } from "./context/RequireAuth.tsx";
 import { NotFound } from "./pages/NotFound.jsx";
 import { Register } from "./pages/Register.tsx";
+import { Animes } from "./pages/privates/Animes.tsx";
+import { Games } from "./pages/privates/Games.tsx";
+import { Home } from "./pages/privates/Home.tsx";
+import { Movies } from "./pages/privates/Movies.tsx";
+import { Series } from "./pages/privates/Series.tsx";
 
 export function App() {
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-      <Toaster
-        toastOptions={{
-          duration: 3000,
-          position: "top-center",
-          style: {
-            backgroundColor: "#151B26",
-            color: "#FEFBFB",
-          },
-        }}
-      />
+      <AuthProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <RequireAuth>
+                  <Home />
+                </RequireAuth>
+              }
+            />
+            <Route
+              path="/movies"
+              element={
+                <RequireAuth>
+                  <Movies />
+                </RequireAuth>
+              }
+            />
+            <Route
+              path="/series"
+              element={
+                <RequireAuth>
+                  <Series />
+                </RequireAuth>
+              }
+            />
+            <Route
+              path="/animes"
+              element={
+                <RequireAuth>
+                  <Animes />
+                </RequireAuth>
+              }
+            />
+            <Route
+              path="/games"
+              element={
+                <RequireAuth>
+                  <Games />
+                </RequireAuth>
+              }
+            />
+            <Route path="/register" element={<Register />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
+        <Toaster
+          toastOptions={{
+            duration: 3000,
+            position: "top-center",
+            style: {
+              backgroundColor: "#151B26",
+              color: "#FEFBFB",
+            },
+          }}
+        />
+      </AuthProvider>
     </>
   );
 }
