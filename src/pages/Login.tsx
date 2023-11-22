209ca8d1ab7b2800d1e2b58bdc6ddc4e7d5bd87f
murilonicemento <murilo_nascimento22@outlook.com>
2023-11-22 20:36:58 -0300

    feat: criação da base da página de login

---
+++ src/pages/Login.tsx
@@ -0,0 +1,11 @@
+import { Header } from "../components/Header.tsx";
+import { LoginForm } from "../components/LoginForm.tsx";
+
+export function Login() {
+  return (
+    <>
+      <Header />
+      <LoginForm />
+    </>
+  );
+}
