e2f12e262560669c3129cf149575c87eec14b5a2
murilonicemento <murilo_nascimento22@outlook.com>
2023-11-22 20:41:00 -0300

    fix: correção de erros de importação de imagem

--- src/pages/privates/Home.tsx
+++ src/pages/privates/Home.tsx
@@ -1,10 +1,10 @@
+import tanjiro from "../../assets/tanjiro.svg";
+import viking from "../../assets/viking.jpg";
 import { Footer } from "../../components/Footer";
 import { Header } from "../../components/Header";
 import { Image } from "../../components/Image";
 import { LatestNewsCard } from "../../components/LatestNewsCard";
 import { ReleaseCard } from "../../components/ReleaseCard";
-import tanjiro from "../assets/tanjiro.svg";
-import viking from "../assets/viking.jpg";
 
 export function Home() {
   return (
