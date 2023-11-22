e06c4fc18ab1300aa7dd75102052913ebccd8811
murilonicemento <murilo_nascimento22@outlook.com>
2023-11-22 20:40:08 -0300

    alteração do nome do componente do formulário

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,11 +1,11 @@
-import { Form } from "../components/Form.tsx";
 import { Header } from "../components/Header.tsx";
+import { RegisterForm } from "../components/RegisterForm.tsx";
 
 export function Register() {
   return (
     <>
       <Header />
-      <Form />
+      <RegisterForm />
     </>
   );
 }
