97e0aa826d1ac77d72aa2dad9ef778174ef26e8d
murilonicemento <murilo_nascimento22@outlook.com>
2023-11-24 19:31:52 -0300

    chore: atualiza imports no código

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./App.tsx";
-import { AuthProvider } from "./context/AuthProvider.tsx";
+import { AuthProvider } from "./context/auth/AuthProvider.tsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
