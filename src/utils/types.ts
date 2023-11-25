5faf715961070a1a9681d0d14a4a154db07c880b
murilonicemento <murilo_nascimento22@outlook.com>
2023-11-25 11:22:17 -0300

    feat: criação de interface dos dados que serão retornados pela API ao realizar o login

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -5,6 +5,14 @@ export interface FormProps {
   passwordRepeat: string;
 }
 
+export interface User {
+  id: number;
+  name: string;
+  email: string;
+  password: string;
+  access_token: string;
+}
+
 export interface RegisterDataAPI {
   user: {
     id: number;
@@ -15,12 +23,16 @@ export interface RegisterDataAPI {
   statusCode: number;
 }
 
-export interface User {
-  id: number;
-  name: string;
-  email: string;
-  password: string;
-  access_token: string;
+export interface LoginDataAPI {
+  user: {
+    id: number;
+    name: string;
+    email: string;
+    password: string;
+    access_token: string;
+  };
+  message: string;
+  statusCode: number;
 }
 
 export interface RegisterErrorAPI {
@@ -30,8 +42,11 @@ export interface RegisterErrorAPI {
   statusCode: number;
 }
 
+export interface LoginErrorAPI extends RegisterErrorAPI {}
+
 export type AuthContextType = {
   user: User | null;
+  validateToken: (url: string, token: string) => Promise<boolean>;
   register: (name: string, email: string, password: string) => Promise<boolean>;
   login: (email: string, password: string) => Promise<boolean>;
   signOut: () => void;
