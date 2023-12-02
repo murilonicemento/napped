a33668b1b9835d3dc9bc0d9052bfc8d31cf25fe8
murilonicemento <murilo_nascimento22@outlook.com>
2023-12-02 17:59:14 -0300

    chore: inclusão TODO

--- src/pages/privates/Animes.tsx
+++ src/pages/privates/Animes.tsx
@@ -32,6 +32,12 @@ export function Animes() {
     console.log(data);
   }
 
+  /**
+   * TODO: Ao invés do botão minha colocar um avatar (foto de perfil do usuário) e ao clicar redirecionar para página de alteração de informações do usuário
+   * TODO: Cria uma página para minha conta onde será possível alterar informações do usuário e deletar a conta. Também adicionar a funcionalidade de inclusão foto de perfil (Colocar como foto padrão Toji Fushiguro)
+   * TODO: Adicionar animações
+   */
+
   useEffect(() => {
     const access_token = localStorage.getItem("authToken");
 
