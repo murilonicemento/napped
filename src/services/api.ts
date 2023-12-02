efd9a2107bd393aebfba657ea140c38639d5cc1c
murilonicemento <murilo_nascimento22@outlook.com>
2023-12-02 16:46:46 -0300

    chore: alteração nome variável

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
 
-export const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL,
+export const authAPI = axios.create({
+  baseURL: import.meta.env.VITE_API_AUTH_BASE_URL,
 });
