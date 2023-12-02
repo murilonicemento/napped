1cec0e2c5fcb56fd3957d8f665df41f7b8152403
murilonicemento <murilo_nascimento22@outlook.com>
2023-12-02 17:18:08 -0300

    feat: inclusão de nova API

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,3 +3,7 @@ import axios from "axios";
 export const authAPI = axios.create({
   baseURL: import.meta.env.VITE_API_AUTH_BASE_URL,
 });
+
+export const newsAPI = axios.create({
+  baseURL: import.meta.env.VITE_API_NEWS_BASE_URL,
+});
