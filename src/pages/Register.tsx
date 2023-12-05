05004674ccc18e55393cac10b87321a97c7a11ce
murilonicemento <murilo_nascimento22@outlook.com>
2023-12-05 20:06:35 -0300

    feat: inclusão de verificação se o usuário já está logado

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,7 +1,27 @@
+import { useContext, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import { Header } from "../components/Header.tsx";
 import { RegisterForm } from "../components/RegisterForm.tsx";
+import { AuthContext } from "../context/auth/AuthContext.tsx";
 
 export function Register() {
+  const auth = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  const validate = async () => {
+    const access_token = localStorage.getItem("authToken");
+
+    if (!access_token) return;
+
+    const isValidated = await auth.validateToken(access_token);
+
+    if (isValidated) return navigate("/");
+  };
+
+  useEffect(() => {
+    validate();
+  }, []);
+
   return (
     <>
       <Header />
