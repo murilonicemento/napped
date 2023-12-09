5e10ee9cd31884f2548eb9b656560a05ec0f9415
murilonicemento <murilo_nascimento22@outlook.com>
2023-12-09 15:58:03 -0300

    chore: alteração para criar botão de redirecionamento

--- src/components/NavLink.tsx
+++ src/components/NavLink.tsx
@@ -1,13 +1,37 @@
-import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { AuthContext } from "../context/auth/AuthContext";
 import { NavLinkProps } from "../utils/types";
 
-export function NavLink({ to, isActive, text }: NavLinkProps) {
-  const defaultClasses =
-    "cursor-pointer hover:text-dark-blue transition-colors";
-  const activeClasses = "text-dark-blue";
+export function NavLink({
+  to,
+  isActive,
+  text,
+  isButton,
+  buttonColor,
+}: NavLinkProps) {
+  const defaultClass = "cursor-pointer hover:text-dark-blue transition-colors";
+  const activeClass = "text-dark-blue";
+  const buttonClass = `w-36 text-white bg-gradient-to-b ${buttonColor} flex justify-center border rounded border-none p-2`;
+  const auth = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  const handleSignOut = () => {
+    auth.signOut();
+
+    return navigate("/login");
+  };
 
   return (
-    <Link to={to} className={`${defaultClasses} ${isActive && activeClasses}`}>
+    <Link
+      to={to}
+      onClick={text === "Sair" ? handleSignOut : undefined}
+      className={
+        isButton
+          ? `${buttonClass}`
+          : `${defaultClass} ${isActive && activeClass}`
+      }
+    >
       {text}
     </Link>
   );
