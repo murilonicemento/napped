b6861dcb980584020e9620ba56bb9b5192338b5d
murilonicemento <murilo_nascimento22@outlook.com>
2023-12-16 16:25:58 -0300

    feat: integração com API da NewsDataIO

--- src/pages/privates/Home.tsx
+++ src/pages/privates/Home.tsx
@@ -1,13 +1,61 @@
+import { useCallback, useEffect, useState } from "react";
 import AnimatedPage from "../../animatedPage";
+import chichiro from "../../assets/images/chichiro.svg";
+import goku from "../../assets/images/goku.svg";
+import kratos from "../../assets/images/kratos.svg";
+import naoko from "../../assets/images/naoko.svg";
 import tanjiro from "../../assets/images/tanjiro.svg";
-import viking from "../../assets/images/viking.jpg";
+import viking from "../../assets/images/viking.svg";
 import { Footer } from "../../components/Footer";
 import { Header } from "../../components/Header";
 import { Image } from "../../components/Image";
 import { LatestNewsCard } from "../../components/LatestNewsCard";
 import { ReleaseCard } from "../../components/ReleaseCard";
+import { newsAPI } from "../../services/api";
+import { NewsDataIO } from "../../utils/types";
 
 export function Home() {
+  const [seriesData, setSeriesData] = useState<NewsDataIO>();
+  const [gamesData, setGamesData] = useState<NewsDataIO>();
+  const [animesData, setAnimesData] = useState<NewsDataIO>();
+  const [moviesData, setMoviesData] = useState<NewsDataIO>();
+
+  const getDataAPI = useCallback(async () => {
+    try {
+      const seriesData = await newsAPI.get(
+        `/news?apikey=${
+          import.meta.env.VITE_API_NEWSDATAIO_KEY
+        }&q=series&language=pt`
+      );
+      const gamesData = await newsAPI.get(
+        `/news?apikey=${
+          import.meta.env.VITE_API_NEWSDATAIO_KEY
+        }&q=game&language=pt`
+      );
+      const animesData = await newsAPI.get(
+        `/news?apikey=${
+          import.meta.env.VITE_API_NEWSDATAIO_KEY
+        }&q=animes&language=pt`
+      );
+      const moviesData = await newsAPI.get(
+        `/news?apikey=${
+          import.meta.env.VITE_API_NEWSDATAIO_KEY
+        }&q=animes&language=pt`
+      );
+
+      setSeriesData(seriesData.data);
+      setGamesData(gamesData.data);
+      setAnimesData(animesData.data);
+      setMoviesData(moviesData.data);
+    } catch (error) {
+      console.error(error);
+    }
+  }, []);
+
+  useEffect(() => {
+    getDataAPI();
+  }, [getDataAPI]);
+
   return (
     <AnimatedPage>
       <Header />
@@ -22,21 +70,33 @@ export function Home() {
       <main className="mt-12">
         <section className="w-10/12 m-auto flex flex-col gap-4">
           <Image
-            imageURL={viking}
-            title="Séries"
-            subtitle="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet."
-            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi."
+            imageURL={
+              seriesData?.results[0].image_url
+                ? seriesData?.results[0].image_url
+                : viking
+            }
+            type="Séries"
+            title={seriesData?.results[0].title}
+            description={seriesData?.results[0].description}
           />
           <div className="flex flex-col gap-4">
             <Image
-              imageURL={viking}
-              title="Séries"
-              description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi."
+              imageURL={
+                gamesData?.results[0].image_url
+                  ? seriesData?.results[0].image_url
+                  : kratos
+              }
+              type="Jogos"
+              title={gamesData?.results[0].title}
             />
             <Image
-              imageURL={viking}
-              title="Séries"
-              description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi."
+              imageURL={
+                animesData?.results[0].image_url
+                  ? seriesData?.results[0].image_url
+                  : chichiro
+              }
+              type="Animes"
+              title={animesData?.results[0].title}
             />
           </div>
         </section>
@@ -44,27 +104,21 @@ export function Home() {
           <h2 className="text-white text-xl mb-12">Notícias mais recentes</h2>
           <LatestNewsCard
             imageURL={tanjiro}
-            title="Séries"
-            subtitle="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet."
-            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi."
+            type="Filmes"
+            title={moviesData?.results[0].title}
+            description={moviesData?.results[0].description}
           />
           <LatestNewsCard
-            imageURL={tanjiro}
-            title="Séries"
-            subtitle="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet."
-            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi."
+            imageURL={naoko}
+            type="Animes"
+            title={animesData?.results[0].title}
+            description={animesData?.results[0].description}
           />
           <LatestNewsCard
-            imageURL={tanjiro}
-            title="Séries"
-            subtitle="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet."
-            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi."
-          />
-          <LatestNewsCard
-            imageURL={tanjiro}
-            title="Séries"
-            subtitle="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet."
-            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi."
+            imageURL={goku}
+            type="Séries"
+            title={seriesData?.results[0].title}
+            description={seriesData?.results[0].description}
           />
         </section>
         <section className="w-10/12 m-auto mt-28">
@@ -72,13 +126,13 @@ export function Home() {
           <div className="flex flex-col gap-8">
             <ReleaseCard
               type="Filmes"
-              title="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
-              description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ornare urna pharetra ut ac, pellentesque. "
+              title={moviesData?.results[0].title}
+              description={moviesData?.results[0].description}
             />
             <ReleaseCard
               type="Filmes"
-              title="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
-              description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ornare urna pharetra ut ac, pellentesque. "
+              title={moviesData?.results[0].title}
+              description={moviesData?.results[0].description}
             />
           </div>
         </section>
