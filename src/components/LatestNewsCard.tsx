cb0ec76a0acf4ca2d1b4c6cd7269c5fb1af16654
murilonicemento <murilo_nascimento22@outlook.com>
2023-12-16 16:26:18 -0300

    chore: alteração nome props

--- src/components/LatestNewsCard.tsx
+++ src/components/LatestNewsCard.tsx
@@ -3,16 +3,16 @@ import { LatestNewsCardProps } from "../utils/types";
 
 export function LatestNewsCard({
   imageURL,
+  type,
   title,
-  subtitle,
   description,
 }: LatestNewsCardProps) {
   return (
     <div className="w-full border border-solid rounded border-dark-30 pb-8 mb-10">
       <img src={imageURL} alt="Image" className="" />
       <div className="w-10/12 m-auto mt-6">
-        <h2 className="text-dark-blue uppercase">{title}</h2>
-        <h3 className="text-white text-xl mt-6">{subtitle}</h3>
+        <h2 className="text-dark-blue uppercase">{type}</h2>
+        <h3 className="text-white text-xl mt-6">{title}</h3>
         <p className="text-dark-40 mt-2">{description}</p>
         <Link to="" className="text-dark-blue mt-4 hover:text-white">
           Ler mais
