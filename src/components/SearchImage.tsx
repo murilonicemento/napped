aa783625163f37220822bb3f14ef275e53d20b6d
murilonicemento <murilo_nascimento22@outlook.com>
2023-12-16 13:57:23 -0300

    chore: inclusão de props e imagens dinâmicas

--- src/components/SearchImage.tsx
+++ src/components/SearchImage.tsx
@@ -1,16 +1,31 @@
-import gojo from "../assets/images/gojo.png";
+import { useLocation } from "react-router-dom";
+import theLastOfUs from "../assets/images/GOF.png";
+import fiveNight from "../assets/images/five-night.jpeg";
+import strangerThings from "../assets/images/stranger-things.jpg";
+import yourName from "../assets/images/your-name.jpg";
 
-export function SearchImage() {
+export function SearchImage({ title }: { title: string }) {
+  const { pathname } = useLocation();
+
+  function handleSourceImage() {
+    switch (pathname) {
+      case "/series":
+        return strangerThings;
+      case "/movies":
+        return fiveNight;
+      case "/animes":
+        return yourName;
+      case "/games":
+        return theLastOfUs;
+    }
+  }
+
   return (
-    <div className="filter brightness-75 relative -z-10">
-      <img
-        src={gojo}
-        alt="Imagem"
-        className="w-full h-fit border border-none rounded-md"
-      />
+    <div className="max-h-72 filter brightness-75 relative -z-10 overflow-hidden rounded-md">
+      <img src={handleSourceImage()} alt="Imagem" className="object-cover" />
       <div className="w-full absolute bottom-0 left-0 right-0 p-4 text-white">
-        <h2 className="uppercase text-sm">Animes</h2>
-        <p className="text-sm mt-2 text-dark-40">
+        <h2 className="uppercase text-sm">{title}</h2>
+        <p className="max-w-[492px] text-sm mt-2 text-dark-40">
           O Naped pode ser sua fonte de informações sobre o mundo nerd e outros
           assuntos relacionados.
         </p>
