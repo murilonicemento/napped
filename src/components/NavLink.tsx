7fa6f8f14cb67fc544b486b4fe53124f56b39595
murilonicemento <murilo_nascimento22@outlook.com>
2023-12-16 11:20:20 -0300

    chore: mudança da estrutura do componente

--- src/components/NavLink.tsx
+++ src/components/NavLink.tsx
@@ -1,38 +1,12 @@
-import { useContext } from "react";
-import { Link, useNavigate } from "react-router-dom";
-import { AuthContext } from "../context/auth/AuthContext";
+import { Link } from "react-router-dom";
 import { NavLinkProps } from "../utils/types";
 
-export function NavLink({
-  to,
-  isActive,
-  text,
-  isButton,
-  buttonColor,
-}: NavLinkProps) {
-  const defaultClass =
-    "text-lg bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500 text-transparent bg-clip-text cursor-pointer hover:text-dark-blue transition-colors";
+export function NavLink({ to, isActive, text }: NavLinkProps) {
+  const defaultClass = "cursor-pointer hover:text-dark-blue transition-colors";
   const activeClass = "text-dark-blue";
-  const buttonClass = `w-36 text-white bg-gradient-to-b ${buttonColor} flex justify-center border rounded border-none p-2`;
-  const auth = useContext(AuthContext);
-  const navigate = useNavigate();
-
-  const handleSignOut = () => {
-    auth.signOut();
-
-    return navigate("/login");
-  };
 
   return (
-    <Link
-      to={to}
-      onClick={text === "Sair" ? handleSignOut : undefined}
-      className={
-        isButton
-          ? `${buttonClass}`
-          : `${defaultClass} ${isActive && activeClass}`
-      }
-    >
+    <Link to={to} className={`${defaultClass} ${isActive && activeClass}`}>
       {text}
     </Link>
   );
