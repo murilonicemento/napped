036c61ff80843e9588c55bf8f40f60fdb0c202f4
murilonicemento <murilo_nascimento22@outlook.com>
2023-12-18 19:22:25 -0300

    style: inclusão de scroll do conteúdo

--- src/components/Image.tsx
+++ src/components/Image.tsx
@@ -1,6 +1,6 @@
 import { ImageProps } from "../utils/types";
 
-export function Image({ imageURL, title, subtitle, description }: ImageProps) {
+export function Image({ imageURL, title, type, description }: ImageProps) {
   return (
     <div className="relative -z-10">
       <img
@@ -8,11 +8,11 @@ export function Image({ imageURL, title, subtitle, description }: ImageProps) {
         alt="Imagem"
         className="w-full h-auto border border-none rounded-md"
       />
-      <div className="w-full absolute bottom-0 left-0 right-0 p-4 text-white bg-gradient-to-t to-gradient-image-to from-gradient-image-from">
+      <div className="w-full h-3/4 overflow-scroll absolute bottom-0 left-0 right-0 p-4 text-white bg-gradient-to-t to-gradient-image-to from-gradient-image-from">
         <span className="w-20 p-1 grid place-items-center bg-gradient-to-b from-brand to-dark-blue border border-none rounded-md">
-          <h2 className="uppercase">{title}</h2>
+          <h2 className="uppercase">{type}</h2>
         </span>
-        <h3 className="text-xl mt-2">{subtitle}</h3>
+        <h3 className="text-xl mt-2">{title}</h3>
         <p className="text-sm mt-2 text-dark-40">{description}</p>
       </div>
     </div>
