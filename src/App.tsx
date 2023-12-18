6f443e799c5c4bf1e62a9a26171481ae62c6f2a6
murilonicemento <murilo_nascimento22@outlook.com>
2023-12-18 20:40:38 -0300

    chore: inclusão chakra provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { ChakraProvider } from "@chakra-ui/react";
 import { AnimatePresence } from "framer-motion";
 import { Route, Routes } from "react-router-dom";
 import { Login } from "./pages/Login.tsx";
@@ -13,6 +14,7 @@ import { Series } from "./pages/privates/Series.tsx";
 export function App() {
   return (
     <>
+      {/* <ChakraProvider> */}
       <AnimatePresence mode="wait">
         <Routes location={location} key={location.pathname}>
           <Route path="/register" element={<Register />} />
@@ -26,6 +28,7 @@ export function App() {
           <Route path="*" element={<NotFound />} />
         </Routes>
       </AnimatePresence>
+      {/* </ChakraProvider> */}
     </>
   );
 }
