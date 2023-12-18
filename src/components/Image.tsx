f56cb7a072728c6985b20c420bd9077fdb0c69d5
murilonicemento <murilo_nascimento22@outlook.com>
2023-12-18 20:25:44 -0300

    style: alteração altura da imagem

--- src/components/Image.tsx
+++ src/components/Image.tsx
@@ -8,7 +8,7 @@ export function Image({ imageURL, title, type, description }: ImageProps) {
         alt="Imagem"
         className="w-full h-auto border border-none rounded-md"
       />
-      <div className="w-full h-3/4 overflow-scroll absolute bottom-0 left-0 right-0 p-4 text-white bg-gradient-to-t to-gradient-image-to from-gradient-image-from">
+      <div className="w-full h-3/4 absolute bottom-0 left-0 right-0 p-4 text-white bg-gradient-to-t to-gradient-image-to from-gradient-image-from">
         <span className="w-20 p-1 grid place-items-center bg-gradient-to-b from-brand to-dark-blue border border-none rounded-md">
           <h2 className="uppercase">{type}</h2>
         </span>
