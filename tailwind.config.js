01fb1a6e55d413d9436f859ec5bb6a409aa1d717
murilonicemento <murilo_nascimento22@outlook.com>
2023-12-21 19:19:00 -0300

    chore: inclusão de breakpoints das telas

--- tailwind.config.js
+++ tailwind.config.js
@@ -28,6 +28,13 @@ export default {
     },
     backgroundPosition: {
       left: "8px"
+    },
+    screens: {
+      "sm": "640px",
+      "md": "768px",
+      "lg": "1024px",
+      "xl": "1280px",
+      "2xl": "1536px",
     }
   },
   plugins: [],
