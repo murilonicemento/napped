57eff0fbff4d553a0b22f68b7aae4dbf1e266f37
murilonicemento <murilo_nascimento22@outlook.com>
2023-12-21 20:11:52 -0300

    style: estilização para telas maiores

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import brandLogo from "../assets/images/brand-logo.svg";
 
 export function Footer() {
   return (
-    <footer className="w-full p-4 bg-footer text-dark-40 flex flex-col items-center justify-center gap-1 fixed bottom-0">
+    <footer className="w-full p-4 bg-footer text-dark-40 flex flex-col items-center justify-center lg:flex-row lg:justify-around gap-1 fixed bottom-0">
       <p>Copyright © 2023. Todos os direitos reservados.</p>
       <div className="flex items-center justify-center gap-2">
         <p>Powered by</p>
