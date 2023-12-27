f4df6b9ef234c2773bc67a923d21a8d3aed58426
murilonicemento <murilo_nascimento22@outlook.com>
2023-12-27 15:23:35 -0300

    remoção componente Loading

--- src/components/Loading.tsx
+++
@@ -1,9 +0,0 @@
-import { Spinner, Stack } from "@chakra-ui/react";
-
-export function Loading() {
-  return (
-    <Stack direction="row" spacing={4}>
-      <Spinner size="xl" />
-    </Stack>
-  );
-}
